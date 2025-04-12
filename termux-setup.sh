4a4a0fa11a7f6f81e5bc1f784f11e4c562939d72
USERMAN7 <tokdlybrawla@gmail.com>
2025-04-12 12:57:46 +0500

    изменено:      termux-setup.sh

--- termux-setup.sh
+++ termux-setup.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/bash
+echo 'Пожалуйста запускайте в директории проекта!'
 echo 'Hi this is setuping proggram...'
 echo 'assuming you have debian based distro, loading dependencies'
 apt update && sudo apt upgrade -y
 apt install gcc g++ git -y
 ./compile.sh
+echo $PWD > .dir
 echo 'Use compiled version in output directory!'
 
