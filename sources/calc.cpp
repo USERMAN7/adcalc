a5240b52400e9d8fea5c813e812bbe0ce709b21a
USERMAN7 <tokdlybrawla@gmail.com>
2025-05-09 17:07:35 +0500

    It's executable now run.sh   	Now using functions instead of hardcoded equations sources/calc.cpp

--- sources/calc.cpp
+++ sources/calc.cpp
@@ -1,99 +1,119 @@
 #include <iostream>
 using namespace std;
-int main() {
-	cout << "Привет это инжернерный калькулятор(без синусов и кос)\n";
-	float a,b;
-	float xy,by,ab;
-	float x,y,g;
-	g = 10;
-	cout << "Хотите сначала попробовать обычную версию?\n";
-	cout << "1: да(калькулятор)" << " 2: нет(калькулятор++)\n";
-	int calc,calcoption;
-	cin >> calc;
-	switch(calc) {
-		case 1: calcoption = 1; break;
-		case 2: calcoption = 2; break;
-		default: return 1;
+void calc1(float a, float b, short unsigned c) {
+	switch(c) {
+		case 1: cout << (a + b); break;
+		case 2: cout << (a - b); break;
+		case 3: cout << (a * b); break;
+		case 4: cout << (a / b); break;
+		default: cout << "Error! Not user fault the proggrammer is just dumb\n"; break;
+	
 	}
-	if(calcoption == 1) {
-		cout << endl << "Отлично вот обычный калькулятор\n";
-		cout << "Введите первое число!:";
-		cin >> a;
-		cout << "\n" << "Введите второе число!:";
-		cin >> b;
-		cout << "\n" << "Какую операцию совершить с данными числами?\n";
-		cout << "1: +" << " 2: -" << " 3: /" << " 4: *\n";
-		calc = 0;
-		cin >> calc;
-		// normal calc
-		switch(calc) {
-			case 1: ab = a + b; break;
-			case 2: ab = a - b; break;
-			case 3: ab = a / b; break;
-			case 4: ab = a * b; break;	
-			}
-		cout << "Вывод операции:" << ab << "\n"; 
-		calc = 0;
-	}
-	else if(calcoption == 2) {
-	      	cout << "Привет это калькулятор с поддержкой физ. формул" << "\n"; 
-		// advanced calculator menu
-	        cout << "Выберите нужную формулу" << "\n";
-       		cout << "1:Кинетическая энергия Ek=mv2/2\n";
- 		cout << "2:Км/час в метр/с\n";
-		cout << "3:Потенциальная энергия En=mgh\n";
-		cout << "4:Момент силы M = f * d\n";
-		cout << "5:Центр массы тел в неравномерном расстояние пример в книге\n";
-		cin >> calc;
-		switch(calc) {
-			case 1:
-			// kinetic energy calculator	
-       	       		cout << "Введи первое число массу M:";
-			cin >> x;
- 			cout << "\n" << "Второе число скорость метры в секунду";
-			cin >> y;
-			xy = 0.5 * x * y;
-			cout << "Ответ:" << xy << " Дж";
-		       	break;
-			case 2: // km/h to m/s
-			cout << "Введите скорость километров в час просто как 10 для примера:";
-			cin >> x;
-			y = x / 3.6;
-			cout << "Это будет " << y << " метров в секунду"; break;
-			case 3:
-			// potential energy calculator
-			cout << "Введите массу объекта в кг:";
-			cin >> x;
-			cout << endl << "Введите растояни над землей в метрах:";
-			cin >> y;
-			by = x * g * y;
-			cout << "\n" << "Потенциальная энергия равна:" << by << " Дж" << "\n"; break;
-			// Калькулятор момента силы
-			case 4:
-			cout << "Введите силы в Ньютонах(Н):";
-			cin >> x;
-			cout << "\nВведите радиус-вектор (метры):";
-			cin >> y;
+}
+void calc2(float x, float y, float z, short unsigned v) {
+	switch(v) {
+		case 1:	z = x * y;
+			cout << z; break;
+		case 2:	z = x / 3.6;
+			cout << z; break;
+		case 3:	z = x * 10 * y;
+			cout << z; break;
+		case 5: float xy,by,ab;
 			xy = x * y;
-			cout << "\nВывод:" << xy << "\n"; break;
-			case 5:
-			cout << "\nПояснение масса2 это объект что лежит дальше от вас также с расстоянием";
-			cout << "\nМасса1 это нулевые координаты например земля";
-			cout << "\nВведите массу2:";
-			cin >> a;
-			cout << "\nВведите расстояние2:";
-			cin >> b;
-			cout << "\nВведите массу1:";
-			cin >> x;
-			xy = a * b;
-			by = x + a;
+			by = z + x;
 			ab = xy / by;
-			cout << "\nЦентр массы находится в " << ab << "метрах\n";
+			cout << ab; break;
+
+
+	}
+}
+int main() {
+	float a = 0; 
+	float b = 0;
+	short unsigned c;
+	cout << "Hello, welcome to the adcalc++\n";
+	cout << "Use simple or adcanced calc?\n";
+	cout << "1:Simple calc a+b etc.. 2:Advanced calc kinetic energy etc.. Input:";
+	short unsigned u;
+	cin >> u;
+	if(u > 2 or u < 1)
+		return 2;
+	switch(u) {
+		case 1: cout << "Which operation would you choose?\n";
+			cout << "1:a+b 2:a-b 3:a*b 4:a/b Input:";
+			cin >> c;
+			if(c < 0 or c > 4) {
+			cout << "Choose valid option";
+			return 1;
+			}
+			cout << "Input a var:";
+			cin >> a;
+			if(c == 1) 
+				cout << a << " + b";
+			else if(c == 2)
+				cout << a << " - b";
+			else if(c == 3)
+				cout << a << " * b";
+			else if(c == 4)
+				cout << a << " / b" ;
+			cout << "\nInput b var:";
+			cin >> b;
+			cout << "You answer:";
+			calc1(a,b,c);
+			cout << "\n"; break;
+		case 2: cout << "Choose formulas\n";
+			cout << "1:Kinetic energy Ek=mv2/2\n";
+			cout << "2:Kilometers per hour to m/s\n";
+			cout << "3:Potencitial energy En=mgh\n";
+			cout << "4:Moment of Force M=F*d\n";
+			cout << "5:Center of mass(read the book)\n";
+			cin >> c;
+			if(c < 1 or c > 5) {
+				cout << "Choose valid option";
+				return 3;
+			}
+			switch(c) {
+				default:cout << "Not ready yet! :(\n";
+					return 4;
+				case 1: cout << "Input mass(kg):";
+					cin >> a;
+					cout << "\nInput metres in second:";
+					cin >> b;
+					cout << "\nAnswer:";
+					calc2(a,b,0,c);
+					cout << " J\n"; break;
+				case 2: cout << "Input km/h:";
+					cin >> a;
+					cout << a << " km/h is ";
+					calc2(a,b,0,c);
+					cout << " m/s\n"; break;
+				case 3: cout << "Input object mass(kg):";
+					cin >> a;
+					cout << "\nInput height of obj(m):";
+					cin >> b;
+					calc2(a,b,0,c); break;
+				case 4: cout << "Input force in Newthon(H):";
+					cin >> a;
+					cout << "\nInput radius-vector(m):";
+					cin >> b;
+					cout << "\nOutput:";
+					calc2(a,b,0,1);
+					cout << "\n";
+				case 5: cout << "Input mass of a second obj(kg):";
+					cin >> a;
+					cout << "\nInput a length:";
+					cin >> b;
+					cout << "\nInput mass 1:";
+					cin >> u;
+					cout << "\nPoint of mass is in the ";
+					calc2(a,b,u,c);
+					cout << " m\n"; break;
+						
+			}
+
 
 		}
-	}
-
-
-
+	
+	
 	return 0;
 }
