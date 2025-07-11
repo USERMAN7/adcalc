0320936fc2ac108d72e65682de724a3b667b59e3
USERMAN7 <tokdlybrawla@gmail.com>
2025-07-11 23:45:08 +0500

    Испраалена ошибка.  compile.sh

--- compile.sh
+++ compile.sh
@@ -2,7 +2,7 @@
 if [ -d /home ]; then
 	CONFIG_PWD=$(cat /home/$(whoami)/.config/adcalc/dir) # Смотрит в файл конфига и переводит путь к файлу проекта, сделано для того чтобы не зависить от симболических команд
 else
-	CONFIG_PWD=$(cat ~/.config/adcalc/dir)
+	CONFIG_PWD="$(cat ~/.config/adcalc/dir)"
+fi
 g++ "$CONFIG_PWD"/sources/calc.cpp -o "$CONFIG_PWD"/output/calc
 echo done compiling
-
