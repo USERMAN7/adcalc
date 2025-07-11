a44e9f2f4d7d0ff85d15abe1bb1ce5dac731a3ad
USERMAN7 <tokdlybrawla@gmail.com>
2025-07-11 22:34:53 +0500

    Исправлены ошибки связаные с termux

--- compile.sh
+++ compile.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
-CONFIG_PWD=$(cat /home/$(whoami)/.config/adcalc/dir) # Смотрит в файл конфига и переводит путь к файлу проекта, сделано для того чтобы не зависить от симболических команд
+if [ -d /home ]; then
+	CONFIG_PWD=$(cat /home/$(whoami)/.config/adcalc/dir) # Смотрит в файл конфига и переводит путь к файлу проекта, сделано для того чтобы не зависить от симболических команд
+else
+	CONFIG_PWD=$(cat ~/.config/adcalc/dir)
 g++ "$CONFIG_PWD"/sources/calc.cpp -o "$CONFIG_PWD"/output/calc
 echo done compiling
 
--- termux-setup.sh
+++ termux-setup.sh
@@ -2,11 +2,11 @@
 echo 'Пожалуйста запускайте в директории проекта!'
 echo 'Hi this is setuping proggram...'
 echo 'loading dependencies'
-apt update && sudo apt upgrade -y
-apt install gcc g++ git -y
+yes | pkg install clang
 mkdir output
+mkdir -p ~/.config/adcalc
+echo $PWD > ~/.config/adcalc/dir
 ./compile.sh
-echo $PWD > .dir
 echo 'Use compiled version in output directory!'
 echo 'Для быстрого запуска запустите ./run.sh это вспомогательный файл'
 chmod +x run.sh
