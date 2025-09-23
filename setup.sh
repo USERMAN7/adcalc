1c1beb2bff19132c7628fef550550f252c7ddc1a
USERMAN7 <tokdlybrawla@gmail.com>
2025-09-23 23:27:23 +0200

    failed to use error()

--- setup.sh
+++ setup.sh
@@ -1,44 +1,60 @@
 #!/usr/bin/bash
-config=/home/$(whoami)/.config/adcalc # Makes config directory for project. to compile
+#config=/home/$(whoami)/.config/adcalc # Makes config directory for project. to compile
 if [ -d $config ]; then
 	rm -rf $(cat $config/dir)/output
 	rm -rf $config && echo Removed previous config file.
-else
-	new=1
 fi
+error() {
+	case $1 in
+		install)
+			echo "Installation failed, might be connectivity problem or etc.."
+			exit 1;;
+		setup)
+			printf "Failed to make "$2"/"$3" fatal!"
+			exit 1;;
+		*)
+			echo "Unknown error!!"
+			exit 1;;
+	esac
+}
 echo 'Hi this is setuping proggram...'
-echo 'Please input your distro ID, supported distro debian,arch,rhel'
 #read -r DISTRO #checking distro then installing dependicies.
 #debians=(debian ubuntu mint kali)
 DISTRO=$(grep "ID=*" /etc/os-release|awk -F_ '{print $1}'|grep "ID="|awk -F= '{print $2}')
 #for 
-if [ "$DISTRO" == "debian" ]; then
-	echo -e "\nUsing debian"
-	echo "Installing packages for ""$DISTRO"" "
-	sudo apt update && sudo apt install gcc g++ -y || { echo "Installation failed,
-       please try again."; exit 1; }	
-elif [ "$DISTRO" == "arch" ]; then
-	echo -e "\nUsing arch"
-	echo "Installing packages for ""$DISTRO"" "
-	yes | sudo pacman -S gcc || { echo "Installation failed, please try again."; exit 1; } 
-elif [ "$DISTRO" == "rhel" ]; then
-	echo -e "\nUsing rhel"
-	echo "Installing packages for ""$DISTRO"" "
-	sudo dnf install gcc -y || { echo "Installation failed, please try again."; exit 1; }
-else
-	echo "Do you have dependicies installed? (gcc,g++)"
-	read -r YES_NO
-	if [ "$YES_NO" == "yes" ]; then
-		echo "going through checks."
-	else
-		echo "sorry, installation of packages for non supported distros is not yet added"; exit 1
-	fi
-fi
+case $DISTRO in
+	arch)
+		printf "Using arch\n"
+		printf "Installing packages for arch\n"
+		yes|sudo pacman -S gcc ||error install;;
+	debian)
+		printf "Using debian\n"
+		printf "Installing packages for debian\n"
+		sudo apt install gcc g++ -y||error install;;
+	ubuntu)
+		printf "Using ubuntu\n"
+		printf "Installing packages for ubuntu\n"
+		sudo apt install gcc g++ -y||error install;;
+	fedora)
+		printf "Using fedora\n"
+		printf "Installing packages for fedora\n"
+		sudo dnf install gcc g++ -y||error install;;
+	kali)
+		printf "Using kali\n"
+		printf "Installing packages for kali\n"
+		sudo apt install gcc g++ -y||error install;;
+	*)
+		printf "Failed to know which OS you're using, try to install dependicies yourself.\n"
+		printf "SHELL:"
+		read -r shell
+		eval $shell;;
+esac
+
 #==============================================================+
 #That all double pipes is just checking for fails then exiting.||
 #Nothing serious					       ||
 #==============================================================+
-mkdir -p "$config" || { echo "Failed to make $config."; exit 1; } 
+mkdir -p "$config" || { error setup "$config" "$config"; exit 1; } 
 echo "$PWD" > "$config"/dir
 mkdir "$(cat "$config"/dir)/output" || { echo "Failed to make $(cat "$config"/dir)/output"; exit 1; }
 
